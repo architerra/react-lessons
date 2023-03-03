573fd0a61281b2c3b08ae1ea8ca4ea1b5bba8a5e
Olexiy Voedylo <architerr@gmail.com>
2023-03-03 17:19:59 +0200

    ch3

--- les3/task1/src/index.jsx
+++ les3/task1/src/index.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import Search from './Search';
 
 const rootElement = document.querySelector('#root');
 
-const Greeting = props => {
-  return (
-    <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className="search__field">
-        <input type="text" className="search__input" />
-        <button className="search__button">Search</button>
-      </div>
-    </div>
-  );
-};
-
-ReactDOM.render(<Greeting name="Tom" />, rootElement);
+ReactDOM.render(<Search name="Tom" />, rootElement);
