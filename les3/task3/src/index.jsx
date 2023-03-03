e36e02461fb0721a1cbddd691011f849b933ae10
Olexiy Voedylo <architerr@gmail.com>
2023-03-03 21:57:48 +0200

    ch3

--- les3/task3/src/index.jsx
+++ les3/task3/src/index.jsx
@@ -5,9 +5,9 @@ import Comment from './Comment';
 
 const rootElement = document.querySelector('#root');
 
-const author = {
+const userInfo = {
   name: 'Tom',
   avatarUrl: 'https://avatars.githubusercontent.com/u/9919?v=4',
 };
 
-ReactDOM.render(<Comment user={author} text="Good job!" date={new Date()} />, rootElement);
+ReactDOM.render(<Comment author={userInfo} text="Good job!" date={new Date()} />, rootElement);
