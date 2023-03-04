4b29376252b62fc1296c64b33cd1055833807b14
Olexiy Voedylo <architerr@gmail.com>
2023-03-04 12:34:23 +0200

    ch3

--- les4/task2/src/App.jsx
+++ les4/task2/src/App.jsx
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <>
       <Clock location="New York" offset={-5} />
-      <Clock location="London" offset={0} />
       <Clock location="Kyiv" offset={2} />
+      <Clock location="London" offset={0} />
     </>
   );
 };
