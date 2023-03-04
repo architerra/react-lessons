1df6ff04013046933e3af73079852807029fb58c
Olexiy Voedylo <architerr@gmail.com>
2023-03-04 13:35:37 +0200

    ch6

--- les4/task2/src/Clock.jsx
+++ les4/task2/src/Clock.jsx
@@ -11,7 +11,7 @@ class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 0,
+      time: getTimeWithOffset(this.props.offset),
     };
 
     setInterval(() => {
