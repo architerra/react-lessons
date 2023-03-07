e231d1a68784d91c17c856df206c138960c26528
Olexiy Voedylo <architerr@gmail.com>
2023-03-07 15:15:25 +0200

    ch2

---
+++ les5/task6/src/ColorPicker.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
+  setText(color) {
+    this.setState({
+      text: color,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div class="picker__title">{this.state.text}</div>
+        <div>
+          <button
+            class="picker__button picker__button_coral"
+            onMouseOver={() => this.setText('coral')}
+            onMouseLeave={() => this.setText('')}
+          ></button>
+          <button
+            class="picker__button picker__button_aqua"
+            onMouseOver={() => this.setText('aqua')}
+            onMouseLeave={() => this.setText('')}
+          ></button>
+          <button
+            class="picker__button picker__button_bisque"
+            onMouseOver={() => this.setText('bisque')}
+            onMouseLeave={() => this.setText('')}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
