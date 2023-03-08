f1868a938a071cd74f31871c2e84c54da57ff1a8
Olexiy Voedylo <architerr@gmail.com>
2023-03-08 16:29:02 +0200

    ch4

--- les6/task4/src/Status.jsx
+++ les6/task4/src/Status.jsx
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import Online from './Online';
 import Offline from './Offline';
 
-class Status extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOnline: false,
-    };
-  }
-
-  render() {
-    return <div className="status">{this.state.isOnline ? <Online /> : <Offline />}</div>;
-  }
-}
+// class Status extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isOnline: false,
+//     };
+//   }
+const Status = ({ isOnline }) => {
+  return <div className="status">{isOnline ? <Online /> : <Offline />}</div>;
+};
 
 export default Status;
