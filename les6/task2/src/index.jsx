dad4c425db8aab3f5afb5f4c869b521f6e7e2e2c
Olexiy Voedylo <architerr@gmail.com>
2023-03-08 13:14:22 +0200

    ch1

--- les6/task2/src/index.jsx
+++ les6/task2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import GoodButton from './GoodButton.jsx';
+import Mailbox from './Mailbox.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<GoodButton />, rootElement);
+ReactDOM.render(<Mailbox unreadMessages={[1, 6]} />, rootElement);
