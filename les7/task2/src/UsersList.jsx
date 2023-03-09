c8c7f0b6bc836e1955a54ae8891e20fee21ced43
Olexiy Voedylo <architerr@gmail.com>
2023-03-09 22:21:18 +0200

    ch1

---
+++ les7/task2/src/UsersList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Component } from 'react';
+import User from './User';
+
+class UsersList extends Component {
+  state = {
+    sorting: null,
+  };
+
+  toggleSorting = () => {
+    const newSorting = this.state.sorting === 'asc' ? 'desc' : 'asc';
+    this.setState({
+      sorting: newSorting,
+    });
+  };
+
+  render() {
+    let usList;
+    if (this.state.sorting) {
+      usList = this.props.users
+        .slice()
+        .sort((a, b) => (this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age));
+    } else {
+      usList = this.props.users;
+    }
+    return (
+      <div>
+        <button class="btn" onClick={this.toggleSorting}>
+          {this.state.sorting || '-'}
+        </button>
+        <ul class="users">
+          {usList.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UsersList;
