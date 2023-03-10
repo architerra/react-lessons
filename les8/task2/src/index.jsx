4497e0759e1ac70ee080e0968a998193e8735c78
Olexiy Voedylo <architerr@gmail.com>
2023-03-10 13:35:18 +0200

    ch1

--- les8/task2/src/index.jsx
+++ les8/task2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import NumbersList from './NumbersList.jsx';
+import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<NumbersList numbers={[11, 33, 44]} />, rootElement);
+ReactDOM.render(<App />, rootElement);
