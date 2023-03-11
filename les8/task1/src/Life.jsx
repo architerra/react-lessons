e5868db02bb82009c3d39d0450613af0971264d9
Olexiy Voedylo <architerr@gmail.com>
2023-03-11 17:52:36 +0200

    ch4

--- les8/task1/src/Life.jsx
+++ les8/task1/src/Life.jsx
@@ -17,6 +17,7 @@ class Life extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+    return true;
   }
 
   componentWillUnmount() {
