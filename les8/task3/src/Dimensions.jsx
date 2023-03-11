72ff82bc9e98dd58ca993f6eb9d2e3f0ffd914a1
Olexiy Voedylo <architerr@gmail.com>
2023-03-11 11:53:25 +0200

    ch2

---
+++ les8/task3/src/Dimensions.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+  constructor(props) {
+    super(props);
+
+    const { innerWidth, innerHeight } = window;
+
+    this.state = {
+      width: innerWidth,
+      height: innerHeight,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+
+    this.setState({
+      width: innerWidth,
+      height: innerHeight,
+    });
+    document.title = `${innerHeight} x ${innerWidth}`;
+  };
+
+  render() {
+    return (
+      <>
+        <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>
+      </>
+    );
+  }
+}
+
+export default Dimensions;
