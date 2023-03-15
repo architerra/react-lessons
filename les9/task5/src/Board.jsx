bbaa59f6f98e80f71a3b0980e659949af73c1463
Olexiy Voedylo <architerr@gmail.com>
2023-03-15 12:40:18 +0200

    ch2

--- les9/task5/src/Board.jsx
+++ les9/task5/src/Board.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Square from './Square';
 
-class Board extends React.Component {
+class Board extends Component {
   renderSquare(i) {
     return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
