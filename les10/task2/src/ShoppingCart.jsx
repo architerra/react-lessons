29ac36ac3aaaee85f2f77f68b47715e416836410
Olexiy Voedylo <architerr@gmail.com>
2023-03-15 18:18:37 +0200

    ch3

--- les10/task2/src/ShoppingCart.jsx
+++ les10/task2/src/ShoppingCart.jsx
@@ -20,10 +20,9 @@ class ShoppingCart extends Component {
 
   render() {
     const count = this.state.cartItems.length;
-    const userName = this.props.userData.firstName;
     return (
       <div className="column">
-        <CartTitle userName={userName} count={count} />
+        <CartTitle userName={this.props.userData.firstName} count={count} />
         <ProductsList cartItems={this.state.cartItems} />
       </div>
     );
