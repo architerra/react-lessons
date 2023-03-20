a6a7a9dd2a104164a3fb1ac209d2f2158c190cee
Olexiy Voedylo <architerr@gmail.com>
2023-03-20 11:25:07 +0200

    ch17

--- les11/task3/src/Expand.jsx
+++ les11/task3/src/Expand.jsx
@@ -22,10 +22,10 @@ class Expand extends Component {
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">{this.props.title}</span>
-          <button className="expand__toggle-btn">
+          <button className="expand__toggle-btn" onClick={this.state.isOpen ? this.hideContent : this.showContent}>
             {this.state.isOpen 
-            ? <i className="fas fa-chevron-up" onClick={this.hideContent} /> 
-            : <i className="fas fa-chevron-down" onClick={this.showContent} />}
+            ? <i className="fas fa-chevron-up"  /> 
+            : <i className="fas fa-chevron-down" />}
           </button>
         </div>
         {this.state.isOpen && <div className="expand__content">{this.props.children}</div>}
