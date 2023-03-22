9741efbd81c92059390ee3e9490a3bf255203558
Olexiy Voedylo <architerr@gmail.com>
2023-03-22 20:47:40 +0200

    ch4

--- les13/task2/src/Products.jsx
+++ les13/task2/src/Products.jsx
@@ -6,12 +6,12 @@ function Products({ match }) {
   return (
     <div className="page__content">
       <h1>Products</h1>
-      <ul clasName="navigation">
-        <li className="navigation__item">
-          <Link to={`${match.url}/book`}>Book</Link>
-        </li>
+      <ul className="navigation">
         <li className="navigation__item">
           <Link to={`${match.url}/ball`}>Ball</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to={`${match.url}/book`}>Book</Link>
         </li>
       </ul>
       <Switch>
